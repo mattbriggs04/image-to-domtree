ea9efb21a5776e047f96801809cc2a06a4cc22f0
Matthew Briggs <matthew.briggs04@gmail.com>
2025-04-28 22:36:37 -0400

    tested with random image generation

--- image_gen.py
+++ image_gen.py
@@ -1,12 +1,18 @@
 from PIL import Image
+import random
+import sys
 
-# Create a new 5x5 image in mode '1' (1-bit pixels, black and white)
-img = Image.new('1', (5, 5))
+if len(sys.argv) != 2:
+    print(f"Usage: python3 {sys.argv[1]} <output file>")
+
+# generate a random black and white bmp image
+width=20
+height=10
+img = Image.new('1', (width, height))
 
 pixels = img.load()
-for y in range(5):
-    for x in range(5):
-        pixels[x, y] = (x + y) % 2  # 0 = black, 1 = white
+for y in range(height):
+    for x in range(width):
+        pixels[x, y] = random.randint(0, 1)
 
-# Save as BMP
-img.save('checkerboard.bmp')
+img.save(str(sys.argv[1]), format='BMP')
