2f540657b02a82e3350684cdc7cc46e8a9ae5c4b
Matthew Briggs <matthew.briggs04@gmail.com>
2025-04-30 01:30:59 -0400

    added test script to give an example

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Just an example, will turn the rainbow jpeg (any file format works)
+# and also generate a corresponding color file that contains 16^3 colors
+python parser.py images/rainbow.jpeg output/rainbow.txt 16
