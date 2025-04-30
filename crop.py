d36c664a847b4b294c8d1f8df6ed5916fe64f7c7
Matthew Briggs <matthew.briggs04@gmail.com>
2025-04-30 00:29:17 -0400

    documented and re-formatted crop

--- crop.py
+++ crop.py
@@ -1,9 +1,13 @@
 from PIL import Image
 
-img = Image.open('images/frame.bmp')
+# this file was used in order to crop the qr code image because it was very large (>1.5MB)
+if __name__ == "__main__":
+    imgfile = "images/frame.bmp"
+    img = Image.open(imgfile)
 
-crop_pixels = 32
-width, height = img.size
+    # the amount of pixels to crop of each side
+    crop_pixels = 32
+    width, height = img.size
+    cropped_img = img.crop( (crop_pixels, crop_pixels, width-crop_pixels, height-crop_pixels) )
 
-cropped_img = img.crop( (crop_pixels, crop_pixels, width-crop_pixels, height-crop_pixels) )
-cropped_img.save('images/cropped_qrcode.bmp')
+    cropped_img.save('images/cropped_qrcode.bmp')
